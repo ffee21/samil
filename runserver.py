7d22b5e0a71497ae5df543c3f3b499fc4cf336ca
ffee21 <ffee21@gmail.com>
2016-08-23 02:42:37 +0900

    added requirements, excel export library test

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 # from waitress import serve
 from samil import app
 
-app.run(debug=True, port=80)
+app.run(debug=True, host="0.0.0.0", port=80)
 # serve(app, port=80)
--- samil/views.py
+++ samil/views.py
@@ -2,9 +2,29 @@ from samil import app
 
 @app.route('/')
 def index():
-    return 'Hello World! db on'
+    return 'Hello World! w/ xlsx'
 
-@app.route('/db')
+@app.route('/xlstest')
+def xlstest():
+    import xlsxwriter
+    from flask import make_response
+    
+    import io
+        
+    output = io.BytesIO()
+    
+    workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+    worksheet = workbook.add_worksheet()
+    worksheet.write(0, 0, 'Hello, world!')
+    workbook.close()
+    
+    output.seek(0)
+    response = make_response(output.read())
+    response.mimetype = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    response.headers["Content-Disposition"] = "attachment; filename=hello.xlsx"
+    return response
+
+@app.route('/dbtest')
 def dbtest():
     import pymysql
     import sys
