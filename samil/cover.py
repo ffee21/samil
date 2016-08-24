24a8b4e367260d093ba44a9abe1e7825c9c5375c
ffee21 <ffee21@gmail.com>
2016-08-24 18:32:29 +0000

    user form parameter pass test

--- samil/cover.py
+++ samil/cover.py
@@ -1,5 +1,5 @@
 from samil import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/cover')
 def cover():
@@ -13,3 +13,9 @@ def login():
     return render_template("login.html",
         navbar=None, customcss=["cover",]
     )
+
+@app.route('/checkpass', methods=['POST'])
+def checkpath():
+    print(request.form.values())
+    passcode = request.form.get('inputPasscode', "_")
+    return render_template("checkpass.html", navbar=None, msg=passcode);
