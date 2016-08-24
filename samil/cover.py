48c06d1c88db2a33683c8af4ccb05a67ac25b47e
ffee21 <ffee21@gmail.com>
2016-08-24 17:56:53 +0000

    make better css for cover page and login page

--- samil/cover.py
+++ samil/cover.py
@@ -4,5 +4,12 @@ from flask import render_template
 @app.route('/cover')
 def cover():
     return render_template("cover.html", 
-        navbar=None
+        navbar=None, customcss=["cover",]
+        # navbar=None
+    )
+    
+@app.route('/login')
+def login():
+    return render_template("login.html",
+        navbar=None, customcss=["cover",]
     )
