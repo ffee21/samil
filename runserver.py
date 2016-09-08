56a54a26040021e97a8031582c0d00ce2ae8f2c5
EC2 Default User <ec2-user@ip-172-31-3-164.ap-northeast-2.compute.internal>
2016-09-08 15:33:33 +0000

    change runserver.py

--- runserver.py
+++ runserver.py
@@ -1,8 +1,8 @@
 from waitress import serve
 from samil import app
 
-# app.run(debug=True, host="0.0.0.0", port=8080)
+app.run(debug=True, host="0.0.0.0", port=80)
 # serve(app, port=8080)
-serve(app)
+# serve(app)
 
 # test
